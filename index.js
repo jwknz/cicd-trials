34c0e5d7e65d7f623ee29c85d72c7e51128ef6f6
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-10-09 08:54:31 +1300

    Intiial Commit - Setup basic express app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Test 1</h1>')
+})
+
+app.get('/testing', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.listen(port, () => console.log(`Server is on using port ${port}...`))
